587eae924998e830e9bb3821ecfec95584e4fa64
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-09 19:52:54 +0300

    Create Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ import {
   Subscribe,
   Footer
 } from "./sections"
+import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section
       className="xl:padding-l wide:padding-r padding-b"
     >
