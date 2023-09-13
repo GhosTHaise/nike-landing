c9239c8301ef25e1d0f78f9e8284bed83d7c7d8e
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-13 19:31:46 +0300

    render image on right side

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images"
 import Button from "../components/Button"
 
 const SuperQuality = () => {
@@ -45,7 +46,16 @@ const SuperQuality = () => {
                 label="View details"
             />
           </div>
-          
+      </div>
+      <div
+            className="flex-1 flex justify-center items-center"
+          >
+            <img 
+              src={shoe8} 
+              alt="shoe8" 
+              width={570}
+              height={522}
+              className="object-contain" />
       </div>
     </section>
   )
