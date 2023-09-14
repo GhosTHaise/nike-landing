471576143101c3e216ee2fa5f766261eeea25746
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-14 19:24:00 +0300

    build specail offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Button = ({label,iconUrl}) => {
+const Button = ({label,iconUrl,backgroundColor,borderColor,textColor}) => {
   return (
     <button
-        className='flex justify-center items-center gap-2 
+        className={`flex justify-center items-center gap-2 
             px-7 py-4 border font-montserrat text-lg leading-none
-            bg-coral-red rounded-full text-white
-            '
+            ${
+              backgroundColor ? 
+                `${backgroundColor} ${borderColor} ${textColor}`
+                :
+                "bg-coral-red  text-white"
+            }
+            rounded-full
+            `}
     >
        {label}
        {
