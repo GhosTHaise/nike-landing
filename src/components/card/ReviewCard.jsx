3b39c92a6ba99029a7ef821920f25d1b1af274e4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-15 20:19:38 +0300

    create review card

---
+++ src/components/card/ReviewCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ReviewCard = ({
+    imgURL,
+    customerName,
+    rating,
+    feedback
+}) => {
+  return (
+    <div>ReviewCard</div>
+  )
+}
+
+export default ReviewCard
