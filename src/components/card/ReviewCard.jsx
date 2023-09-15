dc3f69ad634dc10e6317f758f45617d3aabeb6f4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-15 21:40:29 +0300

    build review card

--- src/components/card/ReviewCard.jsx
+++ src/components/card/ReviewCard.jsx
@@ -16,12 +16,12 @@ const ReviewCard = ({
             alt={customerName} 
             className="rounded-full object-cover w-[120px] h-[120px]" />
         <p
-            className='info-text'
+            className='info-text mt-6 text-center max-w-sm'
         >
             {feedback}
         </p>
         <div
-            className=''
+            className='mt-3 flex justify-center items-center gap-2.5'
         >
             <img 
                 src={star} 
@@ -30,11 +30,16 @@ const ReviewCard = ({
                 height={24} 
                 className='object-contain m-0' />
             <p
-                className=''
+                className='text-xl font-montserrat text-slate-gray'
             >
                 ({rating})
             </p>
         </div>
+        <h3
+             className='font-palanquin mt-1 text-3xl text-center font-bold'
+        >
+            {customerName}
+        </h3>
     </div>
   )
 }
